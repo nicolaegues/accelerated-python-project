4d87adce6f6d964b0cac6a838e8bfd2f9aac2912
Nicola Egües <nicola.egues@gmail.com>
2025-02-21 20:12:49 +0000

    changed setup to include faster compiling flag, though not really a speed difference

--- setup_LL.py
+++ setup_LL.py
@@ -1,7 +1,15 @@
 from distutils.core import setup
 from Cython.Build import cythonize
+from setuptools.extension import Extension
+
+ext_modules = [
+    Extension(
+        "LebwohlLasher_cy",
+        sources=["LebwohlLasher_cy.pyx"],
+      extra_compile_args=['-O3'],
+      extra_link_args=['-O3'] )]
 
 
 setup(name="LebwohlLasher_cy",
-      ext_modules=cythonize("LebwohlLasher_cy.pyx"))
+      ext_modules=cythonize(ext_modules))
 
