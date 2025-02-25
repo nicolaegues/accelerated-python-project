d056abf775cb9f8ea3aa58439c9c9c0c0ae5eedf
Nicola Egües <nicola.egues@gmail.com>
2025-02-25 21:36:39 +0000

    cleaned cython file up to only include relevant function to cythonize. Also realised C's rand() function was breaking my parallel cython code, so modified for consistency

--- parallel_cython/LebwohlLasher_cy_parallel.pyx
+++ parallel_cython/LebwohlLasher_cy_parallel.pyx
@@ -23,13 +23,8 @@ domains alternate between old data and new data.
 SH 16-Oct-23
 """
 
-import sys
-import time
-import datetime
-import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib as mpl
 
+import numpy as np
 cimport numpy as cnp
 from libc.math cimport cos, exp, M_PI
 from libc.stdlib cimport rand, RAND_MAX
@@ -37,149 +32,6 @@ from libc.stdlib cimport rand, RAND_MAX
 cimport cython
 from cython.parallel cimport parallel, prange
 cimport openmp
-
-#=======================================================================
-def initdat(nmax):
-    """
-    Arguments:
-      nmax (int) = size of lattice to create (nmax,nmax).
-    Description:
-      Function to create and initialise the main data array that holds
-      the lattice.  Will return a square lattice (size nmax x nmax)
-	  initialised with random orientations in the range [0,2pi].
-	Returns:
-	  arr (float(nmax,nmax)) = array to hold lattice.
-    """
-    arr = np.random.random_sample((nmax,nmax))*2.0*M_PI
-    return arr
-#=======================================================================
-def plotdat(arr, pflag, nmax):
-    """
-    Arguments:
-	  arr (float(nmax,nmax)) = array that contains lattice data;
-	  pflag (int) = parameter to control plotting;
-      nmax (int) = side length of square lattice.
-    Description:
-      Function to make a pretty plot of the data array.  Makes use of the
-      quiver plot style in matplotlib.  Use pflag to control style:
-        pflag = 0 for no plot (for scripted operation);
-        pflag = 1 for energy plot;
-        pflag = 2 for angles plot;
-        pflag = 3 for black plot.
-	  The angles plot uses a cyclic color map representing the range from
-	  0 to pi.  The energy plot is normalised to the energy range of the
-	  current frame.
-	Returns:
-      NULL
-    """
-
-    cdef: 
-      double[:, :] u = np.cos(arr)
-      double[:, :] v = np.sin(arr)
-      double[:, :] cols = np.zeros((nmax,nmax))
-       
-      double[:] x =  np.arange(nmax, dtype = np.float64)
-      double[:] y =  np.arange(nmax, dtype = np.float64)
-      int i, j
-
-    if pflag==0:
-        return
-
-    if pflag==1: # colour the arrows according to energy
-        mpl.rc('image', cmap='rainbow')
-        for i in range(nmax):
-            for j in range(nmax):
-                cols[i,j] = one_energy(arr,i,j,nmax)
-        norm = plt.Normalize(cols.min(), cols.max())
-    elif pflag==2: # colour the arrows according to angle
-        mpl.rc('image', cmap='hsv')
-        cols = arr%np.pi
-        norm = plt.Normalize(vmin=0, vmax=np.pi)
-    else:
-        mpl.rc('image', cmap='gist_gray')
-        cols = np.zeros_like(arr)
-        norm = plt.Normalize(vmin=0, vmax=1)
-
-    quiveropts = dict(headlength=0,pivot='middle',headwidth=1,scale=1.1*nmax)
-    fig, ax = plt.subplots()
-    q = ax.quiver(x, y, u, v, cols,norm=norm, **quiveropts)
-    ax.set_aspect('equal')
-    plt.show()  
-
-#=======================================================================
-
-def plotdep(energy, order, nsteps, temp): 
-    
-    x = np.arange(nsteps + 1)
-
-    fig, axes = plt.subplots((2), figsize = (7, 9))
-    axes[0].plot(x, energy, color = "black")
-    axes[0].set_ylabel("Reduced Energy U/ε")
-    axes[1].plot(x, order, color = "black")
-    axes[1].set_ylabel("Order Parameter, S")
-
-    for ax in axes: 
-        ax.set_title(f"Reduced Temperature, T* = {temp}")
-        ax.set_xlabel("MCS")
-    
-    current_datetime = datetime.datetime.now().strftime("%a-%d-%b-%Y-at-%I-%M-%S%p")
-    #plt.savefig(f"vs_MCS_plot_{current_datetime}")
-    plt.show()
-    
-#=======================================================================
-
-def savedat(arr, nsteps, Ts, runtime, ratio, energy, order, nmax):
-    """
-    Arguments:
-	  arr (float(nmax,nmax)) = array that contains lattice data;
-	  nsteps (int) = number of Monte Carlo steps (MCS) performed;
-	  Ts (float) = reduced temperature (range 0 to 2);
-	  ratio (float(nsteps)) = array of acceptance ratios per MCS;
-	  energy (float(nsteps)) = array of reduced energies per MCS;
-	  order (float(nsteps)) = array of order parameters per MCS;
-      nmax (int) = side length of square lattice to simulated.
-    Description:
-      Function to save the energy, order and acceptance ratio
-      per Monte Carlo step to text file.  Also saves run data in the
-      header.  Filenames are generated automatically based on
-      date and time at beginning of execution.
-	Returns:
-	  NULL
-    """
-
-    # Create filename based on current date and time.
-    current_datetime = datetime.datetime.now().strftime("%a-%d-%b-%Y-at-%I-%M-%S%p")
-    filename = "LL-Output-{:s}.txt".format(current_datetime)
-    FileOut = open(filename,"w")
-    # Write a header with run parameters
-    print("#=====================================================",file=FileOut)
-    print("# File created:        {:s}".format(current_datetime),file=FileOut)
-    print("# Size of lattice:     {:d}x{:d}".format(nmax,nmax),file=FileOut)
-    print("# Number of MC steps:  {:d}".format(nsteps),file=FileOut)
-    print("# Reduced temperature: {:5.3f}".format(Ts),file=FileOut)
-    print("# Run time (s):        {:8.6f}".format(runtime),file=FileOut)
-    print("#=====================================================",file=FileOut)
-    print("# MC step:  Ratio:     Energy:   Order:",file=FileOut)
-    print("#=====================================================",file=FileOut)
-    # Write the columns of data
-    for i in range(nsteps+1):
-        print("   {:05d}    {:6.4f} {:12.4f}  {:6.4f} ".format(i,ratio[i],energy[i],order[i]),file=FileOut)
-    FileOut.close()
-#=======================================================================
-
-def test_equal(energy): 
-
-    og_energy = np.loadtxt("OG_output.txt", usecols=(2,))
-    curr_energy = np.round(energy.astype(float), 4)
-
-    are_equal = np.array_equal(og_energy, curr_energy)
-
-    if are_equal: 
-        print("The new energy values are the same as the original energy values - all good!")
-    else: 
-        print("The energy values differ from the original - something may have gone wrong. ")
-
-#=======================================================================
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
@@ -242,7 +94,7 @@ def all_energy(cnp.ndarray[cnp.double_t, ndim = 2] arr_, int nmax, int threads):
       int i, j
 
 
-    for i in prange(nmax, nogil = True, num_threads = threads):
+    for i in prange(nmax, nogil=True, num_threads = threads):
         for j in range(nmax):
             enall += one_energy(arr,i,j,nmax)
     return enall
@@ -270,7 +122,6 @@ def get_order(cnp.ndarray[cnp.double_t, ndim = 2] arr, int nmax, int threads):
       cnp.ndarray[cnp.double_t, ndim = 2] Qab = np.zeros((3,3)) #declaring this gave a major speedup btw
 
       cnp.ndarray[cnp.double_t, ndim = 1] eigenvalues
-      cnp.ndarray[cnp.double_t, ndim = 2] eigenvectors
 
       double[:, :] delta = delta_
       double[:, :, :] lab = lab_
@@ -279,14 +130,14 @@ def get_order(cnp.ndarray[cnp.double_t, ndim = 2] arr, int nmax, int threads):
       int a, b, i, j
 
 
-    for a in prange(3, nogil = True, num_threads = threads):
+    for a in range(3):
         for b in range(3):
-            for i in range(nmax):
+            for i in prange(nmax, nogil = True, num_threads = threads):
                 for j in range(nmax):
                     Qab[a,b] += 3*lab[a,i,j]*lab[b,i,j] - delta[a,b]
 
     Qab = Qab/(2*nmax*nmax)
-    eigenvalues,eigenvectors = np.linalg.eig(Qab)
+    eigenvalues = np.linalg.eigh(Qab)[0]
     return max(eigenvalues)
 #=======================================================================
 @cython.boundscheck(False)
@@ -319,11 +170,11 @@ def MC_step( cnp.ndarray[cnp.double_t, ndim = 2] arr_, double Ts, int nmax, int 
         # with temperature.
 
         cnp.ndarray[cnp.double_t, ndim = 2] aran_ = np.random.normal(scale=scale, size=(nmax,nmax))
-        #cnp.ndarray[cnp.double_t, ndim = 2] boltz_random_ = np.random.uniform(0, 1.0, size=(nmax,nmax))
+        cnp.ndarray[cnp.double_t, ndim = 2] boltz_random_ = np.random.uniform(0, 1.0, size=(nmax,nmax))
 
 
         double[:, :] aran = aran_
-        #double[:, :] boltz_random = boltz_random_
+        double[:, :] boltz_random = boltz_random_
         double[:, :] arr = arr_
   
         int i, j
@@ -334,7 +185,7 @@ def MC_step( cnp.ndarray[cnp.double_t, ndim = 2] arr_, double Ts, int nmax, int 
 
 
     for p in range(2):
-      for i in prange(nmax, nogil = True, num_threads = threads):
+      for i in prange(nmax, nogil=True, num_threads = threads):
           for j in range(nmax):
 
             if (i+j)%2 == p:
@@ -351,8 +202,7 @@ def MC_step( cnp.ndarray[cnp.double_t, ndim = 2] arr_, double Ts, int nmax, int 
                   # Now apply the Monte Carlo test 
                   boltz = exp( -(en1 - en0) / Ts )
 
-                  random_value = rand()/RAND_MAX
-                  #random_value = boltz_random[i, j]
+                  random_value = boltz_random[i, j]
                   if boltz >= random_value:
                       accept += 1
                   else:
@@ -360,66 +210,3 @@ def MC_step( cnp.ndarray[cnp.double_t, ndim = 2] arr_, double Ts, int nmax, int 
           
               
     return accept/(nmax*nmax)
-#=======================================================================
-def main(program, nsteps, nmax, temp, pflag, threads):
-
-    """
-    Arguments:
-    program (string) = the name of the program;
-    nsteps (int) = number of Monte Carlo steps (MCS) to perform;
-      nmax (int) = side length of square lattice to simulate;
-    temp (float) = reduced temperature (range 0 to 2);
-    pflag (int) = a flag to control plotting.
-    Description:
-      This is the main function running the Lebwohl-Lasher simulation.
-    Returns:
-      NULL
-    """
-  
-    np.random.seed(seed=42)
-
-    nreps = 1
-
-
-    rep_runtimes = np.zeros(nreps)  # Create array to store the runtimes
-  
-    for rep in range(nreps): 
-
-
-      # Create and initialise lattice
-      lattice = initdat(nmax)
-
-      # Plot initial frame of lattice
-      plotdat(lattice,pflag,nmax)
-
-      # Create arrays to store energy, acceptance ratio and order parameter
-      energy = np.zeros(nsteps+1,dtype=np.float64)
-      ratio = np.zeros(nsteps+1,dtype=np.float64)
-      order = np.zeros(nsteps+1,dtype=np.float64)
-
-      # Set initial values in arrays
-      energy[0] = all_energy(lattice,nmax, threads)
-      ratio[0] = 0.5 # ideal value
-      order[0] = get_order(lattice,nmax, threads)
-
-      # Begin doing and timing some MC steps.
-      initial = openmp.omp_get_wtime() #or time.time()?
-      for it in range(1,nsteps+1):
-          ratio[it] = MC_step(lattice,temp,nmax, threads)
-          energy[it] = all_energy(lattice,nmax, threads)
-          order[it] = get_order(lattice,nmax, threads)
-
-      final = openmp.omp_get_wtime() #or time.time()?
-      runtime = final-initial
-
-      rep_runtimes[rep] = runtime
-
-    
-    # Final outputs
-    print("{}: Size: {:d}, Steps: {:d}, Threads: {:d}, T*: {:5.3f}: Order: {:5.3f}, Mean ratio : {:5.3f}, Time: {:8.6f} s \u00B1 {:8.6f} s".format(program, nmax,nsteps, threads, temp,order[nsteps-1], np.mean(ratio), np.mean(rep_runtimes), np.std(rep_runtimes)))
-    # Plot final frame of lattice and generate output file
-    # savedat(lattice,nsteps,temp,runtime,ratio,energy,order,nmax)
-    plotdat(lattice,pflag,nmax)
-    plotdep(energy, order, nsteps, temp)
-    test_equal(energy)
-#=======================================================================
